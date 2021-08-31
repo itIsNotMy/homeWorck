4cdb7dc8ea43cd8e81e6c0157604c18db9916953
Misha <mixail_y1@mail.ru>
2021-08-31 22:12:11 +0000

    itIsFersCommand

--- src/MyCommand.php
+++ src/MyCommand.php
@@ -8,5 +8,20 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class MyCommand extends Command
 {
-
+    
+    protected function configure()
+    {
+        $this
+            ->setName('MyCommand')
+            ->setDescription('show input')
+            ->addArgument('text', \Symfony\Component\Console\Input\InputArgument::REQUIRED)
+        ;
+    }
+    
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln($input->getArgument('text').' '.'Hello');
+        
+        return Command::SUCCESS;
+    }
 }
