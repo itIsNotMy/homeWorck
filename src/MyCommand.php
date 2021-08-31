c3253cb8f6d865ba300e67aec93611514275ce43
Misha <mixail_y1@mail.ru>
2021-08-31 22:40:23 +0000

    itIsSecondsCommand

--- src/MyCommand.php
+++ src/MyCommand.php
@@ -8,5 +8,26 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class MyCommand extends Command
 {
-
+    protected function configure()
+    {
+        $this
+            ->setName('MyCommand')
+            ->setDescription('show input')
+            ->addArgument('text', \Symfony\Component\Console\Input\InputArgument::REQUIRED)
+            ->addArgument('times', \Symfony\Component\Console\Input\InputArgument::OPTIONAL)
+        ;
+    }
+    
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $i=0;
+        
+        do {
+            $output->writeln($input->getArgument('text'));
+            $i++;
+        } while ($i != $input->getArgument('times') && $input->getArgument('times') != null);
+        
+        
+        return Command::SUCCESS;
+    }
 }
