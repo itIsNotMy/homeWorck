4ea0d2e96602c1d88498c6deeff053061f0dff72
Misha <mixail_y1@mail.ru>
2021-09-01 22:55:22 +0000

    itIsCommitCommand1Command2Command3

--- app.php
+++ app.php
@@ -4,7 +4,6 @@ require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'a
 
 $app = new \Symfony\Component\Console\Application('demo application');
 
-<<<<<<< HEAD
 
 $app->add(new \App\Command1());
 
